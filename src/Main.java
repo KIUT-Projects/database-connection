54fc46031d7b0bd33f2cb4cb23bd3846acfad4b9
uzsoftic <softuzb@gmail.com>
2022-11-03 11:58:42 +0500

    first init

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        Connection con = null;
+
+        try {
+
+            Class.forName("org.postgresql")
+
+        }catch (C){
+
+        }
+    }
+}
